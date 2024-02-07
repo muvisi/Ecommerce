84cb728fdf8739157b16cebe33e7c8582d04a65f
KefahWambai <kefah.ngwei@student.moringaschool.com>
2024-02-07 14:08:36 +0300

    added db.json and added shop component

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -13,73 +13,84 @@ import { ProductService } from '../../service/product.service';
 }) 
 export class ShopComponent {
 
-  isSidePanelVisible: boolean= false;
+  isSidePanelVisible: boolean = false;
   productObj: any = {
     "productId": 0,
     "productSku": "",
     "productName": "",
-    "productsPrice": 0,
+    "productPrice": 0,
     "productDescription": "",
+    "categoryId": 0,
     "createdDate": new Date(),
-     
-  }
-  categoryList: any [] = [];
-  productsList: any [] = [];
+    "productImageUrl": ""
+  };
+  categoryList: any[] = [];
+  productsList: any[] = [];
 
-  constructor(private productSrv: ProductService) {
-    
-  }
+  constructor(private productService: ProductService) {}
+
   ngOnInit(): void {
     this.getProducts();
-    this.getALlCategory();
-  }
-  getProducts() {
-    this.productSrv.getProducts().subscribe((res:any)=>{
-      this.productsList = res.data;
-    })
+    this.getCategories();
   }
 
-  getALlCategory() {
-    this.productSrv.getCategory().subscribe((res:any)=>{
-      this.categoryList = res.data;
-    })
-  }
-  onUpdate() {
-    this.productSrv.saveProduct(this.productObj).subscribe((res:any)=>{
-      debugger;
-      if(res.result) {
-        alert("Product Created");
-        this.getProducts();
-      } else {
-        alert(res.message)
+  getProducts() {
+    this.productService.getProducts().subscribe(
+      (res: any) => {
+        this.productsList = res;
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
       }
-    })
+    );
   }
-  onSave() {
-    this.productSrv.saveProduct(this.productObj).subscribe((res:any)=>{
-      debugger;
-      if(res.result) {
-        alert("Product Updated");
-        this.getProducts();
-      } else {
-        alert(res.message)
+
+  getCategories() {
+    this.productService.getCategories().subscribe(
+      (res: any) => {
+        this.categoryList = res;
+      },
+      (error) => {
+        console.error('Error fetching categories:', error);
       }
-    })
+    );
   }
-  onDelete(item: any) {
-    const isDelete = confirm('Are you Sure want to delte');
-    if(isDelete) {
-      this.productSrv.deleteProduct(item.productId).subscribe((res:any)=>{
-        debugger;
-        if(res.result) {
-          alert("Product Deleted");
-          this.getProducts();
-        } else {
-          alert(res.message)
-        }
-      })
-    }
-  }
+  // onUpdate() {
+  //   this.productSrv.saveProduct(this.productObj).subscribe((res:any)=>{
+  //     debugger;
+  //     if(res.result) {
+  //       alert("Product Created");
+  //       this.getProducts();
+  //     } else {
+  //       alert(res.message)
+  //     }
+  //   })
+  // }
+  // onSave() {
+  //   this.productSrv.saveProduct(this.productObj).subscribe((res:any)=>{
+  //     debugger;
+  //     if(res.result) {
+  //       alert("Product Updated");
+  //       this.getProducts();
+  //     } else {
+  //       alert(res.message)
+  //     }
+  //   })
+  // }
+  // onDelete(item: any) {
+  //   const isDelete = confirm('Are you Sure want to delte');
+  //   if(isDelete) {
+  //     this.productSrv.deleteProduct(item.productId).subscribe((res:any)=>{
+  //       debugger;
+  //       if(res.result) {
+  //         alert("Product Deleted");
+  //         this.getProducts();
+  //       } else {
+  //         alert(res.message)
+  //       }
+  //     })
+  //   }
+  // }
 
   onEdit(item: any) {
     this.productObj = item;
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,51 +1,20 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Constant } from '../constant/constant';
-import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
-
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:3000'; 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  public cartUpdated$: Subject<boolean>  = new Subject();
-
-  getCategory() {
-    return this.http.get(Constant.API_END_POINT + Constant.METHODS.GET_ALL_CATEGORY);
+  getProducts(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/products`);
   }
 
-  getProductsByCategory(id: number) {
-    return this.http.get(Constant.API_END_POINT + Constant.METHODS.GET_ALL_PRODUCT_BY_CATEGORY +  id);
-  }
-
-  getProducts() {
-    return this.http.get(Constant.API_END_POINT + Constant.METHODS.GET_ALL_PRODUCT);
-  }
-
-  saveProduct(obj: any) {
-    return this.http.post(Constant.API_END_POINT + Constant.METHODS.CREATE_PRODUCT, obj);
-  }
-
-  updateProduct(obj: any) {
-    return this.http.post(Constant.API_END_POINT + Constant.METHODS.UPDATE_PRODUCT, obj);
-  }
-
-  deleteProduct(id: any) {
-    return this.http.get(Constant.API_END_POINT + Constant.METHODS.DELETE_PRODUCT + id);
-  }
-
-  addToCart(obj: any) {
-    return this.http.post(Constant.API_END_POINT + Constant.METHODS.ADD_TO_CART, obj);
-  }
-
-  getCartDataByCustId(custId: number) {
-    return this.http.get(Constant.API_END_POINT + Constant.METHODS.GET_CART_BY_CUST + custId);
-  }
-
-  removeProductByCartId(cartId: number) {
-    return this.http.get(Constant.API_END_POINT + Constant.METHODS.REMOVE_CART + cartId);
+  getCategories(): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/categories`);
   }
 }
